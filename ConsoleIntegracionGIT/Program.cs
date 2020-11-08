645a4adcefaed39ef5c806cb407aaa92e35ee151
RICARDO VACA <ricardo.vaca@pucese.edu.ec>
2020-11-08 15:31:14 -0500

    Suma de 5 y 10

--- ConsoleIntegracionGIT/Program.cs
+++ ConsoleIntegracionGIT/Program.cs
@@ -9,6 +9,8 @@ namespace ConsoleIntegracionGIT
             Console.WriteLine("Hello World!");
             //Suma de 5 y 10
             Console.WriteLine("La suma de "+ 5+" + "+10+" es igual a: "+ (5+10));
+            //Suma de 5 y 10
+            Console.WriteLine("La suma de " + 5 + " + " + 10 + " es igual a: " + (5 + 10));
         }
     }
 }
